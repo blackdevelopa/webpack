5e8048cbe031ecbacad98470b017ff60cc932d8a
Black Developa <blackdevelopa.com>
2019-02-21 14:13:40 +0100

    import react

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './myStyle.scss';
+
 const App = () => {
   return (
     <div>
       <h1>This is how you setup Webpack</h1>
+      <img alt="header" src="/dist/images/flower.jpeg" className="appHeader" />
       <p>Simply copy the codes you find here. However ensure you understand what's happening behind the scene</p>
     </div>
   );
