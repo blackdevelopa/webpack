b94b704f353d86b77d3425340bf236b59e22e595
Black Developa <blackdevelopa.com>
2019-02-21 09:03:28 +0100

    added the index html file

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is how you setup Webpack</h1>
+      <p>Simply copy the codes you find here. However ensure you understand what's happening behind the scene</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
